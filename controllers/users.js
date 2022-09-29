9fffedcd4b261526a56bfb908a6cda9264ea08b6
SahTitus <sahtitus58@gmail.com>
2022-09-29 17:38:17 +0000

    updates

--- controllers/users.js
+++ controllers/users.js
@@ -29,16 +29,13 @@ export const signin = async (req, res) => {
   try {
     const existingUser = await User.findOne({ email });
 
-    if (!existingUser)
-      return res.status(404).json({ message: "User not found" });
-
     const isPasswordCorrect = await bcrypt.compare(
       password,
       existingUser.password
     );
 
-    if (!isPasswordCorrect)
-      return res.status(404).json({ message: "Invalid credentials" });
+    if (!existingUser || !isPasswordCorrect)
+    return res.status(404).json({ message: "Invalid credentials", type: 'msg' });
 
     const token = jwt.sign(
       { email: existingUser.email, id: existingUser._id },
@@ -48,7 +45,7 @@ export const signin = async (req, res) => {
 
     res.status(200).json({ result: existingUser, token });
   } catch (error) {
-    res.status(500).json({ message: "Something went wrong." });
+    res.status(500).json({ message: "Something went wrong.", });
   }
 };
 
@@ -58,12 +55,16 @@ export const signup = async (req, res) => {
 
   try {
     const existingUser = await User.findOne({ email });
+    const passwordExist = await User.findOne({ password });
 
     if (existingUser)
-      return res.status(400).json({ message: "User already exists" });
+      return res.status(400).json({ message: "User already exists", type: 'msg' });
+
+    if (passwordExist)
+      return res.status(400).json({ message: "Password already exists", type: 'msg' });
 
     if (password !== confirmPassword)
-      return res.status(400).json({ message: "Password don't match" });
+      return res.status(400).json({ message: "Password doesn't match", type: 'msg' });
 
     const hashedPassword = await bcrypt.hash(password, 12);
 
@@ -93,7 +94,6 @@ export const googleAuth = async (req, res) => {
     const userExist = await User.findOne({ email });
 
     if (userExist) {
-      console.log(email);
       const token = jwt.sign(
         { email: userExist.email, id: userExist._id },
         secret,
